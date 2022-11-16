da9435d53dc92900eca53a9fde398a1f1fe3c488
Tiago Barros <tiagobpires@alu.ufc.br>
2022-11-16 17:19:52 -0300

    add triggers on create database sql

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -224,3 +224,176 @@ ALTER TABLE Professor
 	ADD COLUMN id_curso INTEGER,
 	
 	ADD FOREIGN KEY (id_curso) REFERENCES Curso;
+
+CREATE OR REPLACE FUNCTION checagem_coord() RETURNS trigger AS 
+$BODY$
+    DECLARE 
+	prof_eh_diretor SMALLINT;
+    BEGIN
+	SELECT COUNT(1) 
+    INTO prof_eh_diretor
+	FROM Centro C
+	WHERE C.id_profs = NEW.id_profs;
+	
+ 	IF (prof_eh_diretor = 1) THEN
+		RAISE EXCEPTION 'professor já é diretor';
+	END IF;
+	RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER checagem_coord BEFORE INSERT OR UPDATE ON Curso
+    FOR EACH ROW EXECUTE FUNCTION checagem_coord();
+
+
+CREATE OR REPLACE FUNCTION checagem_diretor() RETURNS trigger AS 
+$BODY$
+    DECLARE 
+	prof_eh_coord SMALLINT;
+    BEGIN
+	SELECT COUNT(1) 
+    INTO prof_eh_coord
+	FROM Curso C
+	WHERE C.id_profs = NEW.id_profs;
+	
+ 	IF (prof_eh_coord = 1) THEN
+		RAISE EXCEPTION 'professor já é coordenador';
+	END IF;
+	RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER checagem_diretor BEFORE INSERT OR UPDATE ON Centro
+    FOR EACH ROW EXECUTE FUNCTION checagem_diretor();
+
+
+CREATE OR REPLACE FUNCTION checagem_matricula_disciplina() RETURNS trigger AS 
+$BODY$
+    DECLARE 
+	aluno_esta_matriculado SMALLINT;
+    BEGIN
+	SELECT COUNT(1) 
+    INTO aluno_esta_matriculado
+    FROM Turma_Aluno TA
+    JOIN Turma T
+    ON T.id_turma = TA.id_turma
+    JOIN Disciplina D
+    ON T.id_disciplina = D.id_disciplina
+    WHERE TA.id_aluno = NEW.id_aluno AND D.id_disciplina = (
+        SELECT D.id_disciplina
+        FROM Disciplina D
+        JOIN Turma T
+        ON D.id_disciplina = T.id_disciplina
+        WHERE T.id_turma = NEW.id_turma
+    );
+	
+ 	IF (aluno_esta_matriculado = 1) THEN
+		RAISE EXCEPTION 'aluno já está matriculado em turma dessa disciplina';
+	END IF;
+	RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER checagem_matricula_disciplina BEFORE INSERT OR UPDATE ON TURMA_ALUNO
+    FOR EACH ROW EXECUTE FUNCTION checagem_matricula_disciplina();
+
+CREATE OR REPLACE FUNCTION adicionar_curso_coordenador() RETURNS trigger AS 
+$BODY$
+    BEGIN
+	
+    UPDATE Professor
+    SET id_curso = NEW.id_curso
+    WHERE id_profs = NEW.id_profs;
+    
+	RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER adicionar_curso_coordenador AFTER INSERT OR UPDATE ON Curso
+    FOR EACH ROW EXECUTE FUNCTION adicionar_curso_coordenador();
+
+
+CREATE OR REPLACE FUNCTION checar_diretor_faz_parte_curso() RETURNS trigger AS 
+$BODY$
+    DECLARE 
+	professor_faz_parte_curso SMALLINT;
+    BEGIN
+	SELECT COUNT(1) 
+    INTO professor_faz_parte_curso
+    FROM Professor
+    WHERE id_curso IN (
+        SELECT id_curso
+        FROM Curso
+        WHERE id_centro = OLD.id_centro
+    ) AND id_profs = NEW.id_profs;
+    
+    IF (professor_faz_parte_curso = 0) THEN
+		RAISE EXCEPTION 'professor não faz parte de algum curso que pertence ao centro';
+    END IF;
+    
+	RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER checar_diretor_faz_parte_curso BEFORE UPDATE ON Centro
+    FOR EACH ROW EXECUTE FUNCTION checar_diretor_faz_parte_curso();
+
+
+CREATE OR REPLACE FUNCTION limiteLeciona() RETURNS trigger AS
+$BODY$
+	DECLARE
+    qnt_disciplinas SMALLINT;
+    
+	BEGIN
+    select count (id_disciplina) into qnt_disciplinas from professor_disciplina where id_profs = NEW.id_profs GROUP BY id_profs  ;
+   	 
+ 	IF (qnt_disciplinas > 4) THEN
+    	RAISE EXCEPTION 'limite de cadeiras excedida';
+	END IF;
+	RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER limiteLeciona BEFORE INSERT ON professor_disciplina
+	FOR EACH ROW EXECUTE FUNCTION limiteLeciona();
+
+
+CREATE OR REPLACE FUNCTION incrementarTurma() RETURNS trigger AS
+$BODY$
+
+BEGIN
+
+	UPDATE turma SET qntd_alunos = qntd_alunos + 1;
+	RETURN NEW;
+
+END;
+
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER incrementarTurma AFTER INSERT ON turma_aluno
+FOR EACH ROW EXECUTE FUNCTION incrementarTurma();
+
+
+CREATE OR REPLACE FUNCTION decrementarTurma() RETURNS trigger AS
+$BODY$
+
+BEGIN
+
+           UPDATE turma SET qntd_alunos = qntd_alunos - 1;
+	RETURN NEW;
+
+END;
+
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER decrementarTurma AFTER DELETE ON turma_aluno
+FOR EACH ROW EXECUTE FUNCTION decrementarTurma();
+
